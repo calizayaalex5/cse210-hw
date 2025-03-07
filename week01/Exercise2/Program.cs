07784787c71744a323f973336d53c7a8a5a35788
calizayaalex5 <calizayaalex5@gmail.com>
2025-03-06 22:42:06 -0300

    assigment complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,53 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("How many points have? ");
+        string points = Console.ReadLine();
+        int grade = int.Parse(points);
+
+        string gradeLetter = "";
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+
+        else if (grade >= 87)
+        {
+            gradeLetter = "B+";
+        }
+
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+        }
+
+        if (grade < 60)
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Try Again.");
+        }
     }
 }
