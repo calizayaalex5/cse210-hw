af23c77af5980cf77d1257fd9ba63ba023d25cc0
calizayaalex5 <calizayaalex5@gmail.com>
2025-03-10 23:21:46 -0300

    assigment completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2025;
+        job2._endYear = 2028;
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Alex Calizaya";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
