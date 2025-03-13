b37e7c1bd6050f71a831ef8b18220c014c2e8102
calizayaalex5 <calizayaalex5@gmail.com>
2025-03-12 22:50:26 -0300

    working in the week2 project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        int userAction = -1;
+
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please Select one of the following choices");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write("What do you like to do? ");
+        userAction = int.Parse(Console.ReadLine());
+
     }
 }
